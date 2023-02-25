b026ce2ef6428ce53497bc9de9a9db4eed7c243e
Alice FQ <alice.fitzsimonsq@gmail.com>
2023-02-25 20:30:32 +0100

    Adding to button functionality
    Script to detect which button has been selected

--- game.js
+++ game.js
@@ -1,18 +1,33 @@
-const gamePattern = [];
+//array storing buttons selected by users
+var userClickedPattern = [];
 
 //array storing colors of buttons
-const buttonNumbers = ["red", "blue", "green", "yellow"];
+var buttonNumbers = ["red", "blue", "green", "yellow"];
+
+//array to push randomly selected color into
+var gamePattern = [];
 
 //generate a random number between 0 and 3
-const nextSequence = () => {
-   var randomNumber = Math.floor(Math.random() * 3);
-   console.log(randomNumber);
-}
-//use the random number to select a color from the buttonNumbers array
-var randomChosenColor = buttonNumbers[randomNumber];
+function nextSequence () {
+   var randomNumber = Math.floor(Math.random() * 4);
+   //use the random number to select a color from the buttonNumbers array
+   var randomChosenColor = buttonNumbers[randomNumber];
+   //push the selected random color to the end of gamePattern array
+   gamePattern.push(randomChosenColor);
+   //select button with same ID as randomChosenColor
+   $('#' + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+   //play sound associated with the selected color
 
-//push the selected random color to the end of gamePattern array
-gamePattern.push(randomChosenColor);
+   var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+   audio.play();
+};
 
-//select button with same ID as randomChosenColor
-$('#' + randomChosenColor).animate({ backgroundColor: "#FFFFFF"}, 1500);
+//handler function
+$('.btn').click(function () {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+});
+
+
+
+
